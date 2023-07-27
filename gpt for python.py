fb98b8bda885e85f4bff04ee427f886b7392def3
Daniel Jing <jpdan727@gmail.com>
2023-07-27 16:31:40 +0800

    Update gpt for python.py

--- gpt for python.py
+++ gpt for python.py
@@ -3,6 +3,6 @@ import openai
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 gpt3 = openai.Model.get("gpt-3.5-turbo")
 completion = openai.Completion.create( engine="gpt-3.5-turbo", 
-             prompt="blablabla", 
+             prompt="blablabla...", 
              max_tokens=100, temperature=0.5, )
 generated_text = completion.text
